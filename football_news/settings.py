3ce118e7f6650e176a7e7cd2f0ed197747bdaf71
ruzzy13 <uzma.zhafira@gmail.com>
2025-09-10 15:21:07 +0700

    masih ada error di main.html

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,6 +32,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "zhafira-uzma41-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [ "https://zhafira-uzma41-footballnews.pbp.cs.ui.ac.id"]
+
 
 # Application definition
 
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
